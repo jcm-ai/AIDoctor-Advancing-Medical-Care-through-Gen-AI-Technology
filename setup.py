b721945dc7c37bb38e567aa7484c855939aaab99
jagadishmali567 <jagadishmali567@gmail.com>
2025-01-11 21:22:21 +0530

    Add setup.py for package configuration and update requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "AI Doctor",
+    version = "0.0.0",
+    author = "Jagadish Mali",
+    author_email = "jagadishmali567@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
